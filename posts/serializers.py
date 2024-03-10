d315d28a545b42e9b65ad6ca3e90a78a1dcd8814
Nirob-Barman <niroborin111@gmail.com>
2024-03-10 09:50:13 +0600

    processing

--- posts/serializers.py
+++ posts/serializers.py
@@ -11,6 +11,7 @@ class CommentSerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name']
